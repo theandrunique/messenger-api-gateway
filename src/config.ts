48dc7a62cb8f831e30d5b290a5eb23aa1cf88e5c
theandrunique <theandrunique@gmail.com>
2025-02-19 17:24:01 +0300

    Refactor config

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,16 @@ import { fromZodError } from "zod-validation-error";
 dotenv.config();
 
 const ConfigSchema = z.object({
-  PORT: z.preprocess((val) => Number(val), z.number().default(3000)),
+  PORT: z.preprocess(
+    (val) => (val ? Number(val) : undefined),
+    z.number().default(3000)
+  ),
   JWKS_URL: z.string().default("https://localhost:8000/.well-known/jwks.json"),
   REDIS_URL: z.string().default("redis://127.0.0.1:6379"),
-  EVENT_BATCH_SIZE: z.preprocess((val) => Number(val), z.number().default(100)),
+  EVENT_BATCH_SIZE: z.preprocess(
+    (val) => (val ? Number(val) : undefined),
+    z.number().default(100)
+  ),
   EVENT_STREAM: z.string().default("gateway-events"),
   CONSUMER_GROUP_NAME: z.string().default("socket-consumers"),
   CONSUMER_NAME: z.string().default(`socket-worker-${os.hostname()}`),
